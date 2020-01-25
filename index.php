b251ebcc9a2920cdc19f3ef8ecbda3fcfffc4b23
useradmin12 <60249202+useradmin12@users.noreply.github.com>
2020-01-25 14:08:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BLACKCODER</title>
+    </head>
+    <body>
+        
+    <section>
+    <style> 
+html, body { background-color: #808080 ; height: 100vh; margin: 0; } .full-height { height: 100vh; } .flex-center { align-items: center; display: flex; justify-content: center; } .position-ref { position: relative; } .content { text-align: center; } .title { font-size: 36px; padding: 20px; } </style> 
+<body align="center" oncontextmenu="return false"> <script type="text/javascript">alert("blackcoder"); </script> 
+<div class="flex-center position-ref full-height"> 
+<div class="content"> <div class="text"> 
+<center>
+<img width="300" height="300" src="https://jolerking675.000webhostapp.com/boy.gif">
+<br>
+<font style='text-shadow: 0px 0px 6px rgb(0, 0, 255), 0px 0px 5px rgb(0, 0, 300), 0px 0px 5px rgb(0, 0, 300); color:green; font -weight:bold;' size="4">
+<i><font color="Black" Size="6">[BLACKCODER]</font>
+<br>
+<font color="lightgreen" size="5">[WE ARE :]</font>
+<br>
+<marquee behavior="alternate" width="85%">
+<font color="#26DDC5" size="4">BLACK_CODER -----> BeginnerGhost -----> Mr.Fearless -------> cl@ym00n -----> Mr.PauseBreak -----> Mr.Inivisible -----> X-Ray</font></marquee>
+<br>
+
+<br>
+
+<!--FLOWER SCRIPT-->
+<script src='https://pastebin.com/raw/8v6kQfn5' type='text/javascript'/></script>
+
+<!--SNOW-->
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+
+</span>
+  <!--LEAF-->
+     <div class="leaf">
+     <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"></img></div>
+      <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"></img></div>
+      <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" ></img></div>
+      <div><img  src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"></img></div>
+       <div> <img src="http://www.pngmart.com/files/1/Transparent-Autumn-Leaves-Falling-PNG.png" height="75px" width="75px"></img></div>
+     <div>   <img src="http://www.pngmart.com/files/1/Realistic-Autumn-Fall-Leaves-PNG.png" height="75px" width="75px"></div>
+     <div><img src="http://cdn.clipart-db.ru/rastr/autumn_leaves_025.png" height="75px" width="75px"></div>
+       
+            <div>  <img src="http://www.pngmart.com/files/1/Fall-Autumn-Leaves-Transparent-PNG.png" height="75px" width="75px"></img></div>
+      <div><img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Pictures-Collage-PNG.png" height="75px" width="75px"></img></div>
+      <div>  <img src="http://www.pngmart.com/files/1/Autumn-Fall-Leaves-Clip-Art-PNG.png" height="75px" width="75px" ></img></div>    
+     </div>
+     
+         
+     </div>
+     </section>
+<style>
+body{
+background-color:#808080;
+}
+
+section{
+height:100%;
+width:100%;
+  position:absolute ; 
+}
+.leaf{
+    position:absolute ;
+    width:100%;
+    height:100%;
+    top:0;
+    left:0;
+}
+.leaf div{
+position:absolute ;
+display:block ;
+}
+.leaf div:nth-child(1){
+    left:20%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-2s;
+
+}
+.leaf div:nth-child(2){
+    left:70%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-4s;
+}
+.leaf div:nth-child(3){
+    left:10%; 
+    animation:fall 20s linear infinite ;
+    animation-delay:-7s;
+    
+}
+.leaf div:nth-child(4){
+    left:50%; 
+   animation:fall 18s linear infinite ; 
+   animation-delay:-5s;
+}
+.leaf div:nth-child(5){
+    left:85%; 
+    animation:fall 14s linear infinite ;
+    animation-delay:-5s;
+}
+.leaf div:nth-child(6){
+    left:65%; 
+    animation:fall 16s linear infinite ;
+    animation-delay:-10s;
+}
+.leaf div:nth-child(7){
+    left:90%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-8s;
+}
+.leaf div:nth-child(8){
+    left:70%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-1s;
+}
+.leaf div:nth-child(9){
+    left:40%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-9s;
+}
+.leaf div:nth-child(10){
+    left:30%; 
+    animation:fall 15s linear infinite ;
+    animation-delay:-5s;
+}
+
+@keyframes fall{
+    0%{
+        opacity:1;
+        top:-10%;
+        transform:rotate(0deg);
+    }
+    20%{
+        opacity:0.8;
+        transform:rotate(45deg);
+    }
+    40%{
+
+        transform:(90deg);
+    }
+    60%{
+        
+       transform:rotate(135deg); 
+    }
+    80%{
+    
+        transform:rotate(180deg);
+    }
+    100%{
+        opacity:0.5;
+        top:110%;
+        transform:(225deg);
+    }
+    }
+
+</style><audio id='audio' autoplay='' loop=''>
+   <source src='https://jolerking675.000webhostapp.com/%E1%80%99%E1%80%94%E1%80%99%E1%80%9A%E1%80%86%E1%80%84---Lwan%20Paung,Lil%20Chan,Yaw%20Yamin%20Oo.mp3' >
+</audio>
+    </body>
+</html>
